03a5bc56e47b649cad6910272e5138853a2b1f26
NellowTCS <neeljaiswal23@gmail.com>
2025-07-12 02:09:47 +0000

    weird

--- src/main/Source/html_render.h
+++
@@ -1,9 +0,0 @@
-#ifndef HTML_RENDER_H
-#define HTML_RENDER_H
-
-#include <lvgl.h>
-#include <tt_app_manifest.h>
-
-void render_html_to_lvgl(AppHandle app, lv_obj_t* parent, const char* html);
-
-#endif
